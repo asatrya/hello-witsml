a013abb0a576160df593fe7d34326d0301d44779
Aditya Satrya <aditya.satrya@gmail.com>
2019-06-25 06:06:34 +0700

    initial commit

---
+++ src/main/java/com/bia/sandbox/App.java
@@ -0,0 +1,13 @@
+package com.bia.sandbox;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
