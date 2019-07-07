104b578f33184ab7f3e293a70e9378f13ba35c7e
Aditya Satrya <aditya.satrya@gmail.com>
2019-07-07 16:32:09 +0700

    read cli arguments

--- src/main/java/com/bia/sandbox/App.java
+++ src/main/java/com/bia/sandbox/App.java
@@ -1,88 +1,103 @@
 package com.bia.sandbox;
 
-import com.google.auth.oauth2.ServiceAccountCredentials;
-import com.google.cloud.bigquery.*;
-import com.hashmapinc.tempus.WitsmlObjects.Util.WitsmlMarshal;
-import com.hashmapinc.tempus.WitsmlObjects.v1411.ObjMudLogs;
-import org.apache.xalan.processor.TransformerFactoryImpl;
-
-import javax.xml.bind.JAXBException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Source;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.stream.StreamSource;
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.xml.bind.JAXBException;
+
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.cloud.bigquery.*;
+import com.hashmapinc.tempus.WitsmlObjects.Util.WitsmlMarshal;
+import com.hashmapinc.tempus.WitsmlObjects.v1411.ObjMudLogs;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        String mudlogsXML = null;
-        try {
-            mudlogsXML = getReturnData("1411/mudLog.xml");
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static void main(String[] args) throws IOException, InterruptedException, ParseException {
+
+        Options options = new Options();
+        options.addOption("o", true, "WITSML object name (ex: mudLogs)");
+        options.addOption("f", true, "WITSML file as input");
+        options.addOption("p", true, "GCP project name");
+        options.addOption("k", true, "GCP service account key");
+        options.addOption("d", true, "BigQuery dataset name");
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse( options, args);
+        String witsmlObject = cmd.getOptionValue("o");
+        String xmlFile = cmd.getOptionValue("f");
+        String gcpProjectName = cmd.getOptionValue("p");
+        String gcpKeyPath = cmd.getOptionValue("k");
+        String dataSetName = cmd.getOptionValue("d");
+
+        System.out.println("Read WITSML object: " + witsmlObject);
+        System.out.println("Read XML file: " + xmlFile);
+        System.out.println("Read GCP project name: " + gcpProjectName);
+        System.out.println("Read GCP service account key: " + gcpKeyPath);
+        System.out.println("Read BigQuery dataset: " + dataSetName);
+
+        if(witsmlObject.equals("mudLogs")){
+
+            String tableName = "mudLogs";
+
+            String mudlogsXML = null;
+            mudlogsXML = Files.lines(Paths.get(xmlFile)).collect(Collectors.joining("\n"));
+
+            ObjMudLogs mudlogs = null;
+            try {
+                mudlogs = WitsmlMarshal.deserialize(mudlogsXML, ObjMudLogs.class);
+                System.out.println(mudlogs.getMudLog().size());
+                System.out.println(mudlogs.getMudLog().get(0).getUid());
+            } catch (JAXBException ex) {
+                ex.printStackTrace();
+            }
+
+            BigQuery bigquery = BigQueryOptions.newBuilder().setProjectId(gcpProjectName)
+                    .setCredentials(ServiceAccountCredentials.fromStream(new FileInputStream(gcpKeyPath)))
+                    .build().getService();
+
+            // Create tables
+            TableId tableId = TableId.of(dataSetName, tableName);
+
+            // check if table exists, create new one if not
+            if(! isExisting(bigquery, dataSetName, tableName)){
+                // Table field definition
+                List<Field> fields = new ArrayList<>(Arrays.asList(Field.of("nameWell", LegacySQLTypeName.STRING),
+                Field.of("nameWellbore", LegacySQLTypeName.STRING)));
+
+                // Table schema definition
+                Schema schema = Schema.of(fields);
+                TableDefinition tableDefinition = StandardTableDefinition.of(schema);
+                TableInfo tableInfo = TableInfo.newBuilder(tableId, tableDefinition).build();
+                Table table = bigquery.create(tableInfo);
+                System.out.println("Table " + table.getFriendlyName() + " has been created.");
+            }else{
+                System.out.println("Table " + tableName + " is already exist.");
+            }
+
+        }else{
+            System.out.println("Object " + witsmlObject + " is not supported yet.");
         }
-
-        ObjMudLogs mudlogs = null;
-        try {
-            mudlogs = WitsmlMarshal.deserialize(mudlogsXML, ObjMudLogs.class);
-            System.out.println(mudlogs.getMudLog().size());
-            System.out.println(mudlogs.getMudLog().get(0).getUid());
-        } catch (JAXBException ex) {
-            ex.printStackTrace();
-        }
-
-        BigQuery bigquery = BigQueryOptions.newBuilder().setProjectId("bia-energy")
-                .setCredentials(ServiceAccountCredentials.fromStream(new FileInputStream("/home/asatrya/Documents/gdrive-gmail/01-BIA/Credentials/bia-energy-abf693d2bbec.json")))
-                .build().getService();
-
-        // Create tables
-        TableId tableId = TableId.of("witsml", "mudLog");
-
-        // Table field definition
-        List<Field> fields = new ArrayList<>(
-                Arrays.asList(
-                        Field.of("nameWell", LegacySQLTypeName.STRING),
-                        Field.of("nameWellbore", LegacySQLTypeName.STRING)
-                )
-        );
-
-        // Table schema definition
-        Schema schema = Schema.of(fields);
-        TableDefinition tableDefinition = StandardTableDefinition.of(schema);
-        TableInfo tableInfo = TableInfo.newBuilder(tableId, tableDefinition).build();
-        Table table = bigquery.create(tableInfo);
     }
 
-    private static String prettyFormat(String input, int indent) {
-        try {
-            Source xmlInput = new StreamSource(new StringReader(input));
-            StringWriter stringWriter = new StringWriter();
-            StreamResult xmlOutput = new StreamResult(stringWriter);
-            TransformerFactory transformerFactory = new TransformerFactoryImpl();
-            Transformer transformer = transformerFactory.newTransformer();
-            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
-            transformer.transform(xmlInput, xmlOutput);
-            return xmlOutput.getWriter().toString();
-        } catch (Exception e) {
-            return e.getMessage();
-        }
+    public static boolean isExisting(BigQuery bqObject, String dataSetName, String tableName) {
+        return getDataset(bqObject, dataSetName).get(tableName) != null;
     }
-
-    private static String getReturnData(String resourcePath) throws IOException {
-        InputStream stream = Thread.currentThread().getContextClassLoader().getResourceAsStream(resourcePath);
-        BufferedReader reader = new BufferedReader(
-                new InputStreamReader(stream));
-        return reader.lines().collect(Collectors.joining(
-                System.getProperty("line.separator")));
+    
+    protected static Dataset getDataset(BigQuery bqObject, String dataSetName) {
+        return bqObject.getDataset(dataSetName);
     }
 }
